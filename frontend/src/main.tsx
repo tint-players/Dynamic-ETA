598e588580912d890218559e3eadaa1a9fff8492
Rudra Bhowmik <ffgamer.rudra123@gmail.com>
2026-09-13 01:02:52 +0530

    Load restored v2 constraint layout styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './route-v2.css'
+import './constraint-layout-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
