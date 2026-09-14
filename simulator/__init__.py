5f39b12b03c6db2838dbabd1ecdcee10352c9d51
Rudra Bhowmik <ffgamer.rudra123@gmail.com>
2026-09-14 18:07:02 +0530

    Expose track-aware batch exporter

--- simulator/__init__.py
+++ simulator/__init__.py
@@ -14,7 +14,11 @@ from .engine import SimulationEngine
 from .network_engine_v4_restrictive import NetworkSimulationEngineV4Restrictive
 from .dataset import label_completed_journey, label_completed_multi_train_journey
 from .exporters import BatchExporter, LiveExporter
-from .track_aware_exporters import TrackAwareParquetTelemetryExporter, TrackBlockVisitExporter
+from .track_aware_exporters import (
+    TrackAwareBatchExporter,
+    TrackAwareParquetTelemetryExporter,
+    TrackBlockVisitExporter,
+)
 from .track_blocks import (
     TrackBlockIdentity,
     enumerate_track_blocks,
@@ -42,6 +46,7 @@ __all__ = [
     "label_completed_multi_train_journey",
     "BatchExporter",
     "LiveExporter",
+    "TrackAwareBatchExporter",
     "TrackAwareParquetTelemetryExporter",
     "TrackBlockVisitExporter",
     "TrackBlockIdentity",
